4f00840639f1cccf0899ef67a9d3bdf5a4ea5461
Nate <ndsunc@gmail.com>
2018-05-29 13:56:33 -0700

    Added edit and delete buttons to roommate cards; added name attributes to all <input>s in #newRoommate; created #updateRoommate with similar template to #newRoommate with some minor edits

--- scripts/roommates/templates.js
+++ scripts/roommates/templates.js
@@ -1,4 +1,4 @@
-function card ({ avatar, username, faction, address: { street, suite, city, zipcode } }) {
+function card ({ avatar, username, faction, address: { street, suite, city, zipcode } }, index) {
   return `
     <article class="card m-2 border border-secondary rounded" style="min-width: 12rem;">
       <img class="card-img-top" src="${avatar}" alt="${username}">
@@ -15,8 +15,13 @@ function card ({ avatar, username, faction, address: { street, suite, city, zipc
           ${city}, ${zipcode}
         </address>
       </section>
+      <div class="card-footer d-flex justify-content-between">
+        <button data-id="${index}" class="btn btn-sm btn-light roommate-edit">Edit</button>
+        <button data-id="${index}" class="btn btn-sm btn-link text-danger roommate-remove">Delete</button>
+      </div>
     </article>
   `
+  // added data attribute data-id to line 20 - this gives unique ID to each card's delete button
 }
 
 function newRoommate () {
@@ -28,15 +33,15 @@ function newRoommate () {
         <div class="form-row">
           <div class="form-group col-md-3">
             <label for="username">Username</label>
-            <input type="text" class="form-control" id="username" placeholder="Username">
+            <input type="text" class="form-control" name="username" id="username" placeholder="Username">
           </div>
           <div class="form-group col-md-3">
             <label for="avatar">Avatar URL</label>
-            <input type="url" class="form-control" id="avatar" placeholder="Avatar">
+            <input type="url" class="form-control" name="avatar" id="avatar" placeholder="Avatar">
           </div>
           <div class="form-group col-md-6">
             <label for="faction">Faction</label>
-            <select id="faction" class="form-control">
+            <select name="faction" id="faction" class="form-control">
               <option selected>- Select Your Faction -</option>
               <option>Hopper Hawks</option>
               <option>Lovelace Lemurs</option>
@@ -47,20 +52,20 @@ function newRoommate () {
         </div>
         <div class="form-group">
           <label for="street">Address</label>
-          <input type="text" class="form-control" id="street" placeholder="1234 Main St">
+          <input type="text" class="form-control" name="street" id="street" placeholder="1234 Main St">
         </div>
         <div class="form-group">
           <label for="suite">Address 2</label>
-          <input type="text" class="form-control" id="suite" placeholder="Apartment, studio, or floor">
+          <input type="text" class="form-control" name="suite" id="suite" placeholder="Apartment, studio, or floor">
         </div>
         <div class="form-row">
           <div class="form-group col-md-8">
             <label for="city">City</label>
-            <input type="text" class="form-control" id="city" placeholder="City or town">
+            <input type="text" class="form-control" name="city" id="city" placeholder="City or town">
           </div>
           <div class="form-group col-md-4">
             <label for="inputZip">Zip</label>
-            <input type="text" class="form-control" id="inputZip" placeholder="5 or 9 digits">
+            <input type="text" class="form-control" name="inputZip" id="inputZip" placeholder="5 or 9 digits">
           </div>
         </div>
 
@@ -71,6 +76,58 @@ function newRoommate () {
   `
 }
 
+function updateRoommate () {
+  return `
+    <section id="new-roommate-form">
+      <h2 class="h5 text-center">Update Roommate</h2>
+      <hr/>
+      <form>
+        <div class="form-row">
+          <div class="form-group col-md-3">
+            <label for="username">Username</label>
+            <input type="text" class="form-control" name="username" id="username" placeholder="Username">
+          </div>
+          <div class="form-group col-md-3">
+            <label for="avatar">Avatar URL</label>
+            <input type="url" class="form-control" name="avatar" id="avatar" placeholder="Avatar">
+          </div>
+          <div class="form-group col-md-6">
+            <label for="faction">Faction</label>
+            <select name="faction" id="faction" class="form-control">
+              <option selected>- Select Your Faction -</option>
+              <option>Hopper Hawks</option>
+              <option>Lovelace Lemurs</option>
+              <option>Turing Tigersharks</option>
+              <option>Von Neumann Velociraptors</option>
+            </select>
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="street">Address</label>
+          <input type="text" class="form-control" name="street" id="street" placeholder="1234 Main St">
+        </div>
+        <div class="form-group">
+          <label for="suite">Address 2</label>
+          <input type="text" class="form-control" name="suite" id="suite" placeholder="Apartment, studio, or floor">
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-8">
+            <label for="city">City</label>
+            <input type="text" class="form-control" name="city" id="city" placeholder="City or town">
+          </div>
+          <div class="form-group col-md-4">
+            <label for="inputZip">Zip</label>
+            <input type="text" class="form-control" name="inputZip" id="inputZip" placeholder="5 or 9 digits">
+          </div>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Update Roommate</button>
+        <button id="edit-roommate" class="btn btn-white">Cancel</button>
+      </form>
+    </section>
+  `
+}
+
 module.exports = {
-  card, newRoommate
+  card, newRoommate, updateRoommate
 }
